4418f24f019d60c24e5f4e1815b8456147f8ab20
nanshiou <72543484+nanshiou@users.noreply.github.com>
2023-03-31 09:56:49 +0800

    Add files via upload

---
+++ vendor/mobiscroll-master/js/i18n/mobiscroll.i18n.zh.js
@@ -0,0 +1,48 @@
+(function ($) {
+    $.mobiscroll.i18n.zh = $.extend($.mobiscroll.i18n.zh, {
+        // Core
+        setText: '確定',
+        cancelText: '取消',
+        clearText: '明確',
+        selectedText: '選',
+        // Datetime component
+        dateFormat: 'yyyy-mm-dd',
+        dateOrder: 'yymmdd',
+        dayNames: ['周日','周一','周二','周三','周四','周五','周六'],
+        dayNamesShort: ['日','一','二','三','四','五','六'],
+        dayText: '日',
+        hourText: '時',
+        minuteText: '分',
+        monthNames: ['1月','2月','3月','4月','5月','6月','7月','8月','9月','10月','11月','12月'],
+        monthNamesShort: ['一','二','三','四','五','六','七','八','九','十','十一','十二'],
+        monthText: '月',
+        secText: '秒',
+        timeFormat: 'HH:ii',
+        timeWheels: 'HHii',
+        yearText: '年',
+        nowText: '當前',
+        pmText: '下午',
+        amText: '上午',
+        // Calendar component
+        dateText: '日',
+        timeText: '時間',
+        calendarText: '日曆',
+        closeText: '關閉',
+        // Daterange component
+        fromText: 'Start',
+        toText: 'End',
+        // Measurement components
+        wholeText: 'Whole',
+        fractionText: 'Fraction',
+        unitText: 'Unit',
+        // Time / Timespan component
+        labels: ['年', '月', '日', '時', '分', '秒', ''],
+        labelsShort: ['Yrs', 'Mths', 'Days', 'Hrs', 'Mins', 'Secs', ''],
+        // Timer component
+        startText: 'Start',
+        stopText: 'Stop',
+        resetText: 'Reset',
+        lapText: 'Lap',
+        hideText: 'Hide'
+    });
+})(jQuery);
